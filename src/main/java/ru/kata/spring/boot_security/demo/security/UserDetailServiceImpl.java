ae8ca11816d96b3ef2ced801d87c2dce5f0f37d5
chipsbl <drozdovkin@bk.ru>
2025-04-11 07:31:38 +0300

    Transactional read

--- src/main/java/ru/kata/spring/boot_security/demo/security/UserDetailServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/security/UserDetailServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
 
@@ -19,6 +20,7 @@ public class UserDetailServiceImpl implements UserDetailsService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
