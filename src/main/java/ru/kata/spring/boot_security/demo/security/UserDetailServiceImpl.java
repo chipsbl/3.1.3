b5a19ad6f9997065f71809f2ebe60abeeab26f3a
chipsbl <drozdovkin@bk.ru>
2025-04-11 07:30:44 +0300

    Delete Transactional on UserDetailService

--- src/main/java/ru/kata/spring/boot_security/demo/security/UserDetailServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/security/UserDetailServiceImpl.java
@@ -4,11 +4,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
 @Service
-@Transactional
 public class UserDetailServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
